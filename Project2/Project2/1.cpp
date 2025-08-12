093612213d08d705389fdd11cf431734e1259f57
yueshuang818 <3303298886@qq.com>
2025-08-12 21:41:24 +0800

    Add files via upload

---
+++ Project2/Project2/1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", 522);
+	printf("hello\n");
+	printf("%C", 'B');
+	printf("%C", 'r');
+	printf("%C", 'o');
+	printf("%C", 'n');
+	printf("%C", 'y');
+	printf("%C\n", 'a');
+}
