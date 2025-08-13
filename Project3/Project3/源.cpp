d2384e1a6fb2013e91ce8c5cdf3651e9dac43741
yueshuang818 <3303298886@qq.com>
2025-08-13 21:50:35 +0800

    Add files via upload

---
+++ Project3/Project3/源.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	printf("%s说现在%d点了该睡觉了\n", "bronya",22);
+	float birthday = 8.18;
+	printf("%f\n", birthday);
+	int a = 111;
+	printf("%d\n", a);
+	printf("%c\n", 'a');
+	printf("%p\n", &a);
+	return 0;
+}
