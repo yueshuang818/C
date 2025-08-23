bb550aa0ee0cef3bd1ebb7d0d0866bfc4c1cf805
yueshuang818 <3303298886@qq.com>
2025-08-23 22:30:25 +0800

    Add files via upload

---
+++ 学习过程/是否青年.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int age = 0;
+	scanf("%d",&age);
+	if(18<age &&age<36)
+	printf("ÇàÄê\n"); 
+	int a = 0;
+	int b = 0;
+	a>5?b=3:b=-3;
+	printf("%d",b);
+	 
+	return 0;
+}
+
